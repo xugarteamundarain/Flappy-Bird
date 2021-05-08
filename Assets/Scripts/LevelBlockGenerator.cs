d9d05616fe87d7d5d21f85d3dfc66753810369e0
Xabier Ugarte Amundarain <xabibaxxugarte@gmail.com>
2021-05-08 22:12:02 +0200

    last laptop changes

---
+++ Assets/Scripts/LevelBlockGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBlockGenerator : MonoBehaviour
+{
+    public LevelBlock levelBlock; // Bloque de nivel a generar
+    public LevelBlock lastLevelBlock; // Ultimo bloque colocado
+    public LevelBlock currentLevelBlock; // Nuevo bloque generado
+
+    private void Start()
+    {
+        AddNewBlock();
+    }
+
+    private void Update()
+    {
+        float camera_x = Camera.main.transform.position.x;
+        float old_x = lastLevelBlock.exitPoint.position.x;
+
+        if (camera_x > old_x + lastLevelBlock.width)
+        {
+            RemoveOldBlock();
+            AddNewBlock();
+        }
+    }
+
+    public void AddNewBlock()
+    {
+        LevelBlock block = (LevelBlock)Instantiate(levelBlock);
+        block.transform.SetParent(this.transform, false); 
+        block.transform.position = new Vector2(lastLevelBlock.exitPoint.position.x + lastLevelBlock.width / 2, lastLevelBlock.exitPoint.position.y);
+        currentLevelBlock = block;
+    }
+
+    public void RemoveOldBlock()
+    {
+        Destroy(lastLevelBlock.gameObject);
+        lastLevelBlock = currentLevelBlock;
+    }
+}
